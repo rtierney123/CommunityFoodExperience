57a76aaaf09a5f0c364d4a366bddb3dc7781ae0a
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2019-04-03 01:18:13 -0400

    Added Location Button and PopUp

---
+++ Assets/Scripts/UI/PopUpButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpButton : MonoBehaviour
+{
+    public Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showPopUp()
+    {
+        Debug.Log("PopUp");
+        img.enabled = true;
+    }
+}

746343c9e37a898e73e05cdb434385001055eb0b
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2019-04-07 12:59:39 -0400

    Set up code for bus route.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,50 +6,56 @@ namespace Movement
     public class GameManager : MonoBehaviour
     {
         public Transform player;
-        private Rigidbody playerRb;
-        public Transform path;
-       
+        public Transform bus;
+        public Transform playerPath;
+        public Transform busPath;
         public int speed;
+
+        private bool busComplete;
         // Start is called before the first frame update
         void Start()
         {
-
+            
+            busComplete = false;
+            StartCoroutine(takeBus());
         }
 
         // Update is called once per frame
         void Update()
         {
-            playerRb = player.GetComponent<Rigidbody>();
+            
         }
 
         public void travelPath()
         {
-            StartCoroutine(movePlayer());
+            StartCoroutine(followPath(player, playerPath));
         }
 
-        IEnumerator movePlayer()
+        IEnumerator takeBus()
         {
-            
+            followPath(bus, busPath);
+            yield return new WaitForSeconds(5);
+        }
+
+        IEnumerator followPath(Transform obj, Transform path)
+        {
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+
             foreach (Transform child in path)
             {
                 
-                Vector2 start = player.position;
+                Vector2 start = obj.position;
                 Vector2 end = child.position;
                 PathObj script = child.GetComponent<PathObj>();
            
                 float step = speed * Time.deltaTime;
                 script.setHit(false);
-                /*
-                while (script.getHit())
-                {
-                    playerRb.velocity = step * (end - start);
-                }
-                */
-                playerRb.velocity = step * (end - start);
+            
+                rb.velocity = step * (end - start);
                 //Debug.Log("Waiting for princess to be rescued...");
                 yield return new WaitUntil(() => script.getHit());
                 //Debug.Log("Princess was rescued!");
-                playerRb.velocity = Vector2.zero;
+                rb.velocity = Vector2.zero;
 
             }
             

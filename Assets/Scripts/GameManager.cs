2ded4cbb40f8634bb7026c442caacc33d841462d
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2019-04-10 14:37:15 -0400

    Got rid of parameters for gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,7 @@ namespace Manage
 {
     public class GameManager : MonoBehaviour
     {
-        public Transform player;
-        public Transform bus;
-        public Transform playerPath;
-        public Transform busPath;
-        public Bus busScript;
-        public Player playerScript;
-        public int speed;
-        private bool routeComplete;
-        private bool onBus;
+ 
         // Start is called before the first frame update
         void Start()
         {

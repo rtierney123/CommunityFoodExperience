99ad65d7f17e5e3c934c4922e3da9fddd0bf8418
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-09-04 19:05:55 -0400

    Changable tabs and Clickable wallet button

---
+++ Assets/Scripts/UI/Button/Wallet/Wallet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallet : MonoBehaviour
+{
+    void start() {
+        this.gameObject.SetActive(false);
+    }
+}

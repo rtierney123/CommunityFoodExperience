5ab3642415570160e81e97301a5bfb27b74745a3
rqiu8 <rqiu8@gatech.edu>
2019-09-04 12:52:40 -0400

    Revert "broken player"
    This reverts commit 939b93dd28e2d7dd0aa9b87b8d60d22694031bd7.

--- Assets/Scripts/Pieces/Player.cs
+++ Assets/Scripts/Pieces/Player.cs
@@ -4,54 +4,46 @@ using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : Monobehavior
+public class Player : GamePiece
 {
- 
-
-    // temp player info. will place in better place later
-    public string name;
-    public string characterDescription = "You are an elderly person (senior citizen) and you live alone on a fixed income of $870 a month from Social Security. Your rent for a small apartment is $600/month and you pay $50/month for electricity, $30 month for a phone and $185/month for two prescriptions that are only partially coverd by Medicare.";
-    public string phoneNum = "(404) 888-9360";
-    public string socialSecurity = "XXX-XX-6789";
-    public string address = "258 W.Real St. Atlanta, GA 30317";
-    public int fixedIncome = 870;
-    public int expenses = 855;
-    public int money = 15;
-    int busTokens = 1;
-    bool vita = false;
-    bool snap = false;
-    bool wic = false;
-    int progress = 0;
-    int carbs = 0;
-    int protein = 0;
-    int fruits = 0;
-    int veggies = 0;
-
-    //string[] foodAcquired = [];
-
-    // view player information
-    public int[] getNutrition()
+    public Transform bus;
+    public Text busButtonText;
+    public Transform foodPantry;
+    bool onBus;
+    // Start is called before the first frame update
+    void Start()
     {
-    // return { carbs, protein, fruits, veggies};
-    return null;
+        pieceTransform = this.GetComponent<Transform>();
     }
-    public int getProgress()
-    {
-        return progress;
-    }
-    
-    //.....
 
-    // update player information
-    public void setMoney(int value)
+    // Update is called once per frame
+    void Update()
     {
-        money = value;
+        
     }
-    public void setBusTokens(int value)
-    {
-        busTokens = value;
-    }
-    
-    //.....
 
+
+    public void travelPath()
+    {
+        StartCoroutine(followPath(pieceTransform, path));
+    }
+
+    public void takeBus()
+    {
+        Bus busScript = bus.GetComponent<Bus>();
+        if (busScript.getStopBus() && !onBus)
+        {
+            this.GetComponent<Image>().enabled = false;
+            onBus = true;
+            busButtonText.text = "Get off bus.";
+        }
+        else
+        {
+            this.GetComponent<Image>().enabled = true;
+            this.GetComponent<Transform>().position = foodPantry.position;
+            onBus = false;
+            busButtonText.text = "Take Bus : 1 token";
+        }
+
+    }
 }

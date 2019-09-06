b4a15f160a71e07bd30f87fa01c61bef4a8fb9b6
tland357 <ikamon357@gmail.com>
2019-09-06 12:47:51 -0400

    Working Camera Pan Buttons

--- Assets/Scripts/CameraPan.cs
+++ Assets/Scripts/CameraPan.cs
@@ -13,25 +13,49 @@ public class CameraPan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.mousePosition.x > Screen.width * 0.9f) {
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
 			MoveLeft();
-		} else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.mousePosition.x < Screen.width * .1f) {
+		} else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
 			MoveRight();
 		}
+		MoveFar();
     }
-
+	bool ConstantMoving;
+	bool RightLeft;
+	public void SetRight() {
+		ConstantMoving = RightLeft = true;
+	}
+	public void SetLeft() {
+		ConstantMoving = true;
+		RightLeft = false;
+	}
 	void MoveRight() {
-		if (transform.position.x >= 0.1) {
+		if (transform.position.x >= 0.1f) {
 			var temp = transform.position;
 			temp.x -= .1f;
 			transform.position = temp;
 		}
 	}
 	void MoveLeft() {
-		if (transform.position.x <= 9.6) {
+		if (transform.position.x <= 9.6f) {
 			var temp = this.transform.position;
 			temp.x += .1f;
 			this.transform.position = temp;
+		}
+	}
+	void MoveFar() {
+		if (ConstantMoving) {
+			if (!RightLeft) {
+				MoveRight ();
+				if (this.transform.position.x > 9.6f) {
+					ConstantMoving = false;
+				}
+			} else {
+				MoveLeft ();
+				if (this.transform.position.x < 0.1f) {
+					ConstantMoving = false;
+				}
+			}
 		}
 	}
 }

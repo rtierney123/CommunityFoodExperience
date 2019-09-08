3da3e597a45714cf31b9db441fadf775e623976f
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-09-08 00:00:57 -0400

    Nutrition button

---
+++ Assets/Scripts/UI/Button/Wallet/Nutrition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nutrition : MonoBehaviour
+{
+    public GameObject nutrition;
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in nutrition.transform) {
+            if (child.name == "CaloriesValue") {
+                child.gameObject.GetComponent<Text>().text = player.calories.ToString() + "/" + player.requiredCalories.ToString();
+            } else if (child.name == "GrainValue") {
+                child.gameObject.GetComponent<Text>().text = player.grain.ToString() + "/" + player.requiredGrain.ToString();
+            } else if (child.name == "FatValue") {
+                child.gameObject.GetComponent<Text>().text = player.fat.ToString() + "/" + player.requiredFat.ToString();
+            } else if (child.name == "ProteinValue") {
+                child.gameObject.GetComponent<Text>().text = player.protein.ToString() + "/" + player.requiredProtein.ToString();
+            } else if (child.name == "DairyValue") {
+                child.gameObject.GetComponent<Text>().text = player.dairy.ToString() + "/" + player.requiredDairy.ToString();
+            } else if (child.name == "FruitValue") {
+                child.gameObject.GetComponent<Text>().text = player.fruit.ToString() + "/" + player.requiredFruit.ToString();
+            } else if (child.name == "VegetableValue") {
+                child.gameObject.GetComponent<Text>().text = player.vegetabele.ToString() + "/" + player.requiredVegetable.ToString();
+            } else if (child.name == "ExtraValue") {
+                child.gameObject.GetComponent<Text>().text = player.extra.ToString() + "/" + player.requiredExtra.ToString();
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/Button/Wallet/NutritionButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manage;
+
+public class NutritionButton : MonoBehaviour
+{
+    public GameObject nutrition;
+    public CanvasController canvasController;
+    
+    public void nutritionButtonClicked() {
+        if(!nutrition.activeInHierarchy){
+            canvasController.openPopup(nutrition);
+        } else {
+            canvasController.closePopUp();
+        }
+    }
+
+
+}

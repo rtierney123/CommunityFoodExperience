f41af60cdc6e3796a6003ab3eb11f844bd0ec81d
Rachel Tierney <tierne_r@outlook.com>
2019-09-19 16:21:01 -0400

    Added store and popup script to prevent store from closing on click.

---
+++ Assets/Scripts/Models/Popups/PopUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+
+    public class PopUp : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            setChildrentoParent();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void setChildrentoParent()
+        {
+            foreach (Transform child in this.gameObject.transform)
+
+                child.gameObject.tag = this.gameObject.tag;
+    
+        }
+    }
+
+
+}
+
---
+++ Assets/Scripts/Models/Popups/Store.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI { 
+    public class Store : PopUp
+    {
+      
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+

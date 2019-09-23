bc30cbbbe67c3931144ad70379c214377d8a987d
Rachel Tierney <tierne_r@outlook.com>
2019-09-23 19:18:47 -0400

    Added populategrid script

---
+++ Assets/Scripts/UI/PopulateGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject foodObject;
+    public int numberToCreate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        populateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void populateGrid()
+    {
+        GameObject newObject;
+        for(int i = 0; i < numberToCreate; i++)
+        {
+            newObject = (GameObject)Instantiate(foodObject, transform);
+        }
+    }
+}

868f520d98a3033a07176e64077477cb9a80c253
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-09-26 01:45:40 -0400

    Update character stat after purchase

--- Assets/Scripts/Models/Cart.cs
+++ Assets/Scripts/Models/Cart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System;
 
 public class Cart : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class Cart : MonoBehaviour
     public Text totalText;
     public HashSet<Food> foodInCart;
     private double totalPrice;
-    /*
+    
     private double fruit;
     private double veg;
     private double grain;
@@ -20,8 +21,35 @@ public class Cart : MonoBehaviour
     private double protein;
     private double extra;
     private double calories;
-    */
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public double getFruit() {
+        return fruit;
+    }
+    public double getVeg() {
+        return veg;
+    }
+    public double getGrain() {
+        return grain;
+    }
+    public double getFat() {
+        return fat;
+    }
+    public double getDairy() {
+        return dairy;
+    }
+    public double getProtein() {
+        return protein;
+    }
+    public double getExtra() {
+        return extra;
+    }
+    public double getCalories() {
+        return calories;
+    }
+  
     public void Start()
     {
         foodInCart = new HashSet<Food>();
@@ -54,16 +82,7 @@ public class Cart : MonoBehaviour
 
     private void addItem(Food food) {
         totalPrice += food.cost;
-        updateTotal();
-
-        GameObject icon = populateGrid.addCartItem(food.cartObject);
-        foodInCart.Add(food);
-
-        Transform minusObject = icon.transform.GetChild(0);
-        Button minusButton = minusObject.GetComponent<Button>();
-        minusButton.onClick.AddListener(() => removeItemFromCart(food, icon));
-
-        /*
+        totalPrice = Math.Round(totalPrice * 100) / 100;
         fruit += food.fruit;
         veg += food.veg;
         grain += food.grain;
@@ -72,23 +91,43 @@ public class Cart : MonoBehaviour
         protein += food.protein;
         extra += food.extra;
         calories += food.calories;
-        */
+        totalText.text = totalPrice.ToString();
+
+        // updateTotal();
+
+        GameObject icon = populateGrid.addCartItem(food.cartObject);
+        foodInCart.Add(food);
+
+        Transform minusObject = icon.transform.GetChild(0);
+        Button minusButton = minusObject.GetComponent<Button>();
+        minusButton.onClick.AddListener(() => removeItemFromCart(food, icon));
+
+        
     }
 
     private void removeItemFromCart(Food food, GameObject icon)
     {
         totalPrice -= food.cost;
-        updateTotal();
+        totalPrice = Math.Round(totalPrice * 100) / 100;
+        fruit -= food.fruit;
+        veg -= food.veg;
+        grain -= food.grain;
+        fat -= food.fat;
+        dairy -= food.dairy;
+        protein -= food.protein;
+        extra -= food.extra;
+        calories -= food.calories;
+        totalText.text = totalPrice.ToString();
+        // updateTotal();
 
         populateGrid.removeItem(icon);
         foodInCart.Remove(food);
 
         foodInCart.Remove(food);
-
     }
 
-    private void updateTotal()
-    {
-        totalText.text = totalPrice.ToString();
-    }
+    // private void updateTotal()
+    // {
+    //     totalText.text = totalPrice.ToString();
+    // }
 }
---
+++ Assets/Scripts/UI/Button/Purchase/CompletePurchaseCash.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Manage;
+using System;
+
+public class CompletePurchaseCash : MonoBehaviour
+{
+    public Text cashValue;
+    public Text ctcValue;
+    public Text eitcValue;
+    public Text snapValue;
+
+    public CanvasController canvasController;
+    public GameObject playerGameObject;
+    public GameObject cartGameObject;
+    Player player;
+    Cart cart;
+
+    double cash;
+    double ctc;
+    double eitc;
+    double snap;
+
+    public void pay() {
+        if (canvasController != null)
+        {
+            canvasController.closePopUp();
+        }
+        cash = string.IsNullOrEmpty(cashValue.text) ? 0 : Convert.ToDouble(cashValue.text);
+        ctc = string.IsNullOrEmpty(ctcValue.text) ? 0 : Convert.ToDouble(ctcValue.text);
+        eitc = string.IsNullOrEmpty(eitcValue.text) ? 0 : Convert.ToDouble(eitcValue.text);
+        snap = string.IsNullOrEmpty(snapValue.text) ? 0 : Convert.ToDouble(snapValue.text);
+        player = playerGameObject.GetComponent<Player>();
+        player.money -= cash;
+        player.ctcFunds -= ctc;
+        player.eitcFunds -= eitc;
+        player.snapFunds -= snap;
+        roundAllCurrencies();
+        paySucceed();
+    }
+
+    private void paySucceed() {
+        cart = cartGameObject.GetComponent<Cart>();
+        player.calories += cart.getCalories();
+        player.fruit = cart.getFruit();
+        player.fat = cart.getFat();
+        player.protein = cart.getProtein();
+        player.dairy = cart.getDairy();
+        player.extra = cart.getExtra();
+        player.vegetabele = cart.getVeg();
+        player.grain = cart.getGrain();
+    }
+
+    public void roundAllCurrencies() {
+        player.money = Math.Round(player.money * 100) / 100;
+        player.ctcFunds = Math.Round(player.ctcFunds * 100) / 100;
+        player.eitcFunds = Math.Round(player.eitcFunds * 100) / 100;
+        player.snapFunds = Math.Round(player.snapFunds * 100) / 100;
+    }
+}

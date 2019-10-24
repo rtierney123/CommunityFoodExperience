cfb4004e37ee52234c26b565c4a63889ae14136d
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-10-23 23:09:59 -0400

    Field for form

--- Assets/Scripts/Enum/FormQuestionTypes.cs
+++ Assets/Scripts/Enum/FormQuestionTypes.cs
@@ -4,47 +4,53 @@ using UnityEngine;
 
 public enum FormQuestionType
 {
+    // Name
     FullName = 0,
-    Address = 1,
-    Phone = 2,
-    Num_Children = 3,
-    Monthly_Income = 4,
-    Children_Age = 5,
-    Aid = 6,
-    Single = 7,
-    Married = 8,
-    Joint_Tax = 9,
-    City = 13,
-    State = 14,
-    Zip = 15,
-    // string either WIC or SNAP, leave empty string "" if have none
-    Federal_Assistance = 16,
     First_Name = 17,
     Last_Name = 18,
     Middle_Name = 19,
-    Annual_Income = 20,
-    In_US_More_Than_Six_Month = 21,
-    All_Income_In_US = 22,
-    Income_Less_than_3150 = 23,
+
+    // Address
+    Address = 1,
+    City = 13,
+    State = 14,
+    Zip = 15,
+
+    // General Information
+    Phone = 2,
+    Num_Children = 3,
+    Single = 7,
+    Married = 8,
     // Leave empty if does not have
     SNN = 24,
     DOB = 25,
+    Sex = 29,
+    Primary_Language = 30,
+
+    // string either WIC or SNAP, leave empty string "" if have none
+    Federal_Assistance = 16,
+    Is_Employed = 33,
+    Hourly_Wage = 35,
+    Monthly_Income = 4,
+    Annual_Income = 20,
+    All_Income_In_US = 22,
+    In_US_More_Than_Six_Month = 21,
+    In_US_Legally = 34,
+    Income_Less_than_3150 = 23,
+
     Num_In_Household = 26,
     Signature = 27,
 
-    // Date for signing
+    // Date when sign the paper
     Date = 28,
-    Sex = 29,
-    Primary_Language = 30,
     Live_In_Public_Housing = 31,
     Is_Pregnant = 32,
-    Is_Employed = 33,
-    In_US_Legally = 34,
-    Hourly_Wage = 35,
     Proof_Of_Transportation = 36,
     Has_Fleeing_Member = 37,
     Has_Mispresent_Identity_Member = 38,
     Has_Guity_Drug_Member = 39,
-    Has_Guity_Foodstamp_Member = 40
-
+    Has_Guity_Foodstamp_Member = 40,
+    Joint_Tax = 9,
+    Children_Age = 5,
+    Aid = 6
 }

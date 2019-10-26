efe76cb26470b49b11a016ad27e801cd1e03df6b
Rachel Tierney <tierne_r@outlook.com>
2019-10-26 17:59:44 -0400

    Changed Shelf so that can get json in webgl

--- Assets/Scripts/UI/Shelf.cs
+++ Assets/Scripts/UI/Shelf.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 using Utility;
 
@@ -19,25 +20,65 @@ public class Shelf : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string mainPath = Application.dataPath;
-        jsonLocation = mainPath + jsonLocation;
-        bool pathExists = ResourceHandler.testFilePath(jsonLocation);
-        if (pathExists)
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
-            string json = File.ReadAllText(jsonLocation);
-            foods = JsonUtility.FromJson<FoodList>(json);
-            
-
-            foreach (Food food in foods.list)
-            {
-                grid.addItem(food);
-            }
-
+            jsonLocation = "/StreamingAssets" + jsonLocation;
+            Debug.Log(jsonLocation);
+            StartCoroutine(GetRequest(jsonLocation));
         }
+        else
+        {
+            string mainPath = Application.dataPath;
+            jsonLocation = mainPath + "/StreamingAssets" + jsonLocation;
+            Debug.Log(jsonLocation);
+            bool pathExists = ResourceHandler.testFilePath(jsonLocation);
+            if (pathExists)
+            {
+                string json = File.ReadAllText(jsonLocation);
+                generateFoodAssets(json);
 
+            }
+        }
 
     }
 
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                Debug.Log(webRequest.downloadHandler.text);
+                string json = webRequest.downloadHandler.text;
+
+                generateFoodAssets(json);
+            }
+        }
+    }
+
+    public void generateFoodAssets(string jsonText)
+    {
+        foods = JsonUtility.FromJson<FoodList>(jsonText);
+
+
+        foreach (Food food in foods.list)
+        {
+            grid.addItem(food);
+        }
+    }
 
     [Serializable]
     public class FoodList

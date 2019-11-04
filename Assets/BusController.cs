bec6e53f93099b63321962b4850609f440405e9e
tland357 <ikamon357@gmail.com>
2019-11-04 13:07:11 -0500

    Bus highlight and pausable animation

---
+++ Assets/BusController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusController : MonoBehaviour
+{
+
+	public Animator animator;
+	public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void Pause()
+	{
+		animator.enabled = false;
+	}
+
+	public void Play()
+	{
+		animator.enabled = true;
+	}
+
+	void OnMouseOver()
+	{
+		//if bus can be highlighed condition here
+		material.color = new Color(0xff, 0xf9, 0x82);
+	}
+
+	void OnMouseExit()
+	{
+		material.color = Color.black;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

70a3a7b6fed7b470f95e8b92c6e5f6d805366c62
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-11-07 01:47:50 -0500

    Start sceen and Pause works with clock and bus

---
+++ Assets/Scripts/Screens/StartScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UI;
+using Manage;
+
+public class StartScreen : MonoBehaviour
+{
+    public CanvasController canvasController;
+    // public ClockDisplay clock;
+    public GameObject startScreen;
+
+    void Update() {
+        if (canvasController) {
+            canvasController.pause();
+        }
+    }
+
+    public void startButtonClicked() {
+        canvasController.resume();
+        startScreen.SetActive(false);
+    }
+
+}

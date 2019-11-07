8bba02f1afff80e45c734c300a7442e0c37ccf2f
Tuan Nguyen <atuannguyen1101@gmail.com>
2019-11-07 01:54:01 -0500

    Game with start sceen.

--- Assets/Scripts/Screens/StartScreen.cs
+++ Assets/Scripts/Screens/StartScreen.cs
@@ -10,11 +10,17 @@ public class StartScreen : MonoBehaviour
     // public ClockDisplay clock;
     public GameObject startScreen;
 
-    void Update() {
-        if (canvasController) {
-            canvasController.pause();
-        }
+    void Start() {
+        print("WOW");
+        canvasController.pause();
     }
+
+    // void Update() {
+    //     if (canvasController) {
+    //         print("WOW");
+    //         canvasController.pause();
+    //     }
+    // }
 
     public void startButtonClicked() {
         canvasController.resume();

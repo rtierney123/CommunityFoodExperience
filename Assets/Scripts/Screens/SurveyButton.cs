a81f38d1aa3d68fe954caea8f9e795ee8eaca774
Dean Sun <lsun31@gatech.edu>
2019-11-10 11:45:44 -0500

    endgame/surveybutton

---
+++ Assets/Scripts/Screens/SurveyButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UI;
+using Manage;
+
+public class SurveyButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenURL()
+      {
+          Application.OpenURL("https://forms.gle/qm4gYLffZMdzGeBb6");
+      }
+}

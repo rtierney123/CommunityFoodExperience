4cd2385338bb6160a66219d49dc6e1ff9fbae1ac
Rachel Tierney <tierne_r@outlook.com>
2019-11-19 18:31:35 -0500

    Added expand and contract button for footer.

---
+++ Assets/DisplayFooterButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayFooterButton : MonoBehaviour
+{
+    public Sprite collapseImage;
+    public Sprite expandImage;
+    public Image buttonImage;
+
+    private bool expanded = true;
+
+    public void newState()
+    {
+        if (expanded)
+        {
+            closeFooter();
+            expanded = false;
+        } else
+        {
+            openFooter();
+            expanded = true;
+        }
+    }
+
+    private void openFooter()
+    {
+        buttonImage.sprite = collapseImage;
+    }
+
+    private void closeFooter()
+    {
+        buttonImage.sprite = expandImage;
+    }
+}

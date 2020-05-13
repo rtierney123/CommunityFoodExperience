009c89ad5c7d2f42c5199145c2d13b26696a2d65
Tierney, Rachel Nicole <rtierney3@gatech.edu>
2020-05-13 13:01:26 -0400

    Added health status

--- Assets/Scripts/UI/Button/Wallet/Nutrition.cs
+++ Assets/Scripts/UI/Button/Wallet/Nutrition.cs
@@ -1,66 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Nutrition : MonoBehaviour
+namespace UI
 {
-    public Player player;
-    public Color standardColor;
-    public Color successColor;
-
-    public Text caloriesText;
-    public Text grainText;
-    public Text fatText;
-    public Text proteinText;
-    public Text dairyText;
-    public Text fruitText;
-    public Text vegText;
-    public Text extraText;
-
-    public GameObject plusSignPopup;
-    // Start is called before the first frame update
-    void Start()
+    public class Nutrition : PopUp
     {
-        updateInfo();
-    }
+        public GameObject myPlateTab;
+        public GameObject myPlateTabBody;
+        public GameObject healthStatusTab;
+        public GameObject healthStatusTabBody;
 
-    private void OnEnable()
-    {
-        updateInfo();
-    }
+        public Player player;
+        public Color standardColor;
+        public Color successColor;
 
-    public void updateInfo()
-    {
-        caloriesText.text = player.getCaloriesStatus();
-        grainText.text = player.getGrainStatus();
-        fatText.text = player.getFatStatus();
-        proteinText.text = player.getProteinStatus();
-        dairyText.text = player.getDairyStatus();
-        fruitText.text = player.getFruitStatus();
-        vegText.text = player.getVegetableStatus();
-        extraText.text = player.getExtraStatus();
+        public Text caloriesText;
+        public Text grainText;
+        public Text fatText;
+        public Text proteinText;
+        public Text dairyText;
+        public Text fruitText;
+        public Text vegText;
+        public Text extraText;
+        public Text healthStatusText;
 
-        displaySuccess(caloriesText, player.getAchieveCalories());
-        displaySuccess(grainText, player.getAchieveGrain());
-        displaySuccess(fatText, player.getAchieveFat());
-        displaySuccess(proteinText, player.getAchieveProtein());
-        displaySuccess(dairyText, player.getAchieveDairy());
-        displaySuccess(fruitText, player.getAchieveFruit());
-        displaySuccess(vegText, player.getAchieveVegetable());
-        displaySuccess(extraText, player.getAchieveExtra());
-    }
+        private Color selectColor;
+        private Color inactiveColor;
 
-    private void displaySuccess(Text text, bool success)
-    {
-        if (success)
+        public GameObject plusSignPopup;
+        // Start is called before the first frame update
+
+        protected override void Start()
         {
-            text.color = successColor;
-        }
-        else
-        {
-            text.color = standardColor;
-        }
-    }
+            base.Start();
+            selectColor = new Color32(192, 118, 5, 255);
+            inactiveColor = new Color32(255, 150, 0, 255);
+            setAllInactive();
+            updateInfo();
+            selectMyPlate();
 
+        }
+
+        private void OnEnable()
+        {
+            updateInfo();
+            updateHealthStatus();
+        }
+
+        private void setAllInactive()
+        {
+            myPlateTab.GetComponent<Image>().color = inactiveColor;
+            healthStatusTab.GetComponent<Image>().color = inactiveColor;
+            myPlateTabBody.SetActive(false);
+            healthStatusTabBody.SetActive(false);
+        }
+
+        public void selectMyPlate()
+        {
+            setAllInactive();
+            myPlateTab.GetComponent<Image>().color = selectColor;
+            myPlateTabBody.SetActive(true);
+        }
+
+        public void selectHealthStatus()
+        {
+            setAllInactive();
+            healthStatusTab.GetComponent<Image>().color = selectColor;
+            healthStatusTabBody.SetActive(true);
+
+        }
+
+        public void updateInfo()
+        {
+            caloriesText.text = player.getCaloriesStatus();
+            grainText.text = player.getGrainStatus();
+            fatText.text = player.getFatStatus();
+            proteinText.text = player.getProteinStatus();
+            dairyText.text = player.getDairyStatus();
+            fruitText.text = player.getFruitStatus();
+            vegText.text = player.getVegetableStatus();
+            extraText.text = player.getExtraStatus();
+
+            displaySuccess(caloriesText, player.getAchieveCalories());
+            displaySuccess(grainText, player.getAchieveGrain());
+            displaySuccess(fatText, player.getAchieveFat());
+            displaySuccess(proteinText, player.getAchieveProtein());
+            displaySuccess(dairyText, player.getAchieveDairy());
+            displaySuccess(fruitText, player.getAchieveFruit());
+            displaySuccess(vegText, player.getAchieveVegetable());
+            displaySuccess(extraText, player.getAchieveExtra());
+        }
+
+        public void updateHealthStatus()
+        {
+            //TODO: set actual health status once added to JSON and player
+            healthStatusText.text = "Your community member does not have a heath condition.";
+        }
+
+        private void displaySuccess(Text text, bool success)
+        {
+            if (success)
+            {
+                text.color = successColor;
+            }
+            else
+            {
+                text.color = standardColor;
+            }
+        }
+
+    }
 }

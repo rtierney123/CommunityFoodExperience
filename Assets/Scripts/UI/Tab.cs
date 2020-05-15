16db31b837698ba3d849fbf62047c688506af4ea
Tierney, Rachel Nicole <rtierney3@gatech.edu>
2020-05-15 12:00:32 -0400

    Finished tab display

---
+++ Assets/Scripts/UI/Tab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tab : MonoBehaviour
+{
+    public GameObject expandableContent;
+    public GameObject expandButton;
+    public GameObject collapseButton;
+    public void expand()
+    {
+        expandableContent.SetActive(true);
+        expandButton.SetActive(false);
+        collapseButton.SetActive(true);
+    }
+
+    public void collapse()
+    {
+        expandableContent.SetActive(false);
+        expandButton.SetActive(true);
+        collapseButton.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/TabController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabController : MonoBehaviour
+{
+
+    public Tab[] tabs;
+
+    public void expandTab(Tab selectedTab)
+    {
+        foreach(Tab tab in tabs)
+        {
+            tab.collapse();
+        }
+        selectedTab.expand();
+    }
+
+    public void collapseTab(Tab selectedTab)
+    {
+        selectedTab.collapse();
+    }
+}

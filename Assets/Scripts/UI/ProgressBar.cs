74a7b60ad030b82a01ace5d276214207f8a5f9aa
Tierney, Rachel Nicole <rtierney3@gatech.edu>
2020-05-15 15:45:54 -0400

    Added bus waiting screen.

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public float loadTime;
+    public int displayIntervals;
+    public Slider slider;
+
+    private float currentTime = 0;
+    private bool loading = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (this.gameObject.activeInHierarchy && !loading)
+        {
+            StartCoroutine(Load());
+            Debug.Log("Start Load");
+            loading = true;
+        }
+        else if(!this.gameObject.activeInHierarchy)
+        {
+            currentTime = 0;
+            slider.value = 0;
+            loading = false;
+            Debug.Log("Not Active");
+        }
+
+    }
+
+    public IEnumerator Load()
+    {
+        while(currentTime < loadTime)
+        {
+            float incrementTime = loadTime / displayIntervals;
+            yield return new WaitForSeconds(incrementTime);
+            currentTime += incrementTime;
+            slider.value = (float)(currentTime/ loadTime);
+        }
+
+    }
+
+    public bool getComplete()
+    {
+        return currentTime >= loadTime;
+    }
+
+}

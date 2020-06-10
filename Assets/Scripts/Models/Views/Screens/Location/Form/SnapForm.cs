4493ad4a627cff257619931e9d04631a07c147ab
Rachel Tierney <tierne_r@outlook.com>
2020-06-10 15:59:42 -0400

    Revert "Revert "small fixes""
    This reverts commit 5b766aa41e6ffe5468e0e48012543c6dc5d7c92a.

--- Assets/Scripts/Models/Views/Screens/Location/Form/SnapForm.cs
+++ Assets/Scripts/Models/Views/Screens/Location/Form/SnapForm.cs
@@ -30,34 +30,35 @@ namespace UI
             double monthlyIncome = playerInfo.getTotalIncome();
 
             bool valid = true;
+
             switch (numHouseHold)
             {
-                case 0:
-                    if (monthlyIncome > 1316)
-                    {
-                        valid = false;
-                    }
-                    break;
                 case 1:
-                    if (monthlyIncome > 1784)
+                    if (monthlyIncome > 1000)
                     {
                         valid = false;
                     }
                     break;
                 case 2:
-                    if (monthlyIncome > 2252)
+                    if (monthlyIncome > 2440)
+                    {
+                        valid = false;
+                    }
+                    break;
+                case 3:
+                    if (monthlyIncome > 2440)
                     {
                         valid = false;
                     }
                     break;
                 case 4:
-                    if (monthlyIncome > 2720)
+                    if (monthlyIncome > 2440)
                     {
                         valid = false;
                     }
                     break;
                 default:
-                    if (monthlyIncome > 3188)
+                    if (monthlyIncome > 3120)
                     {
                         valid = false;
                     }
@@ -76,16 +77,31 @@ namespace UI
         {
             double monthlyIncome = playerInfo.getTotalIncome();
             double snapAmt = 0;
-            if(monthlyIncome <= 799)
+
+            if(monthlyIncome <= 1000)
             {
                 snapAmt = 4.5;
-            } else if (monthlyIncome <= 1000 && monthlyIncome>= 800)
+            }
+            else
             {
-                snapAmt = 2.5;
-            } else 
-            {
-                snapAmt = 1;
-            } 
+                int numHouseHold = playerInfo.numInHouse;
+                switch (numHouseHold)
+                {
+                    case 2:
+                        snapAmt = 2.75;
+                        break;
+                    case 3:
+                        snapAmt = 3;
+                        break;
+                    case 4:
+                        snapAmt = 3.75;
+                        break;
+                    default:
+                        snapAmt = 4;
+                        break;
+
+                }
+            }
 
             currencyManager.addFunds(FundsType.Snap, snapAmt);
             string successStr = formatSuccessString(monthlyIncome, snapAmt);
